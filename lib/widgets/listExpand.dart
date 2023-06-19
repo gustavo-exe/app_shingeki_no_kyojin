f6f623d0a78c2ed401af9f68c1bd56396a03e1ca
gustavo-exe <gustavoexe136@gmail.com>
2023-06-18 20:09:24 -0600

    Cards del ejercito

--- lib/widgets/listExpand.dart
+++ lib/widgets/listExpand.dart
@@ -8,23 +8,53 @@ class ListExpand extends StatefulWidget {
 }
 
 class _ListExpandState extends State<ListExpand> {
+  double widthPantalla = 0;
+
   @override
   Widget build(BuildContext context) {
+    widthPantalla = MediaQuery.of(context).size.width;
+
     return Expanded(
-      child: ListView(children: [
-        Text("Episodios"),
+      child: ListView(padding: const EdgeInsets.all(16), children: [
+        const Text(
+          "Ajercito",
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
         Row(
-          children: [cardTeam("exploracion.png", "", "")],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            cardEjercito("exploracion.png", "Cuerpo de Exploración", ""),
+            cardEjercito("militar.png", "Policía Militar", ""),
+            cardEjercito("guarnicion.png", "Tropas de Guarnición", ""),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 6, bottom: 6),
+          child: Divider(
+            thickness: 1,
+            color: Colors.white30,
+          ),
         )
       ]),
     );
   }
 
-  Column cardTeam(String image, String titulo, String subtitulo) {
+  Column cardEjercito(String image, String titulo, String subtitulo) {
     return Column(children: [
-      ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Image.asset("assets/exploracion.png"))
+      Padding(
+        padding: const EdgeInsets.only(top: 4),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset("assets/ejercito/$image",
+                width: widthPantalla * 0.30, fit: BoxFit.cover)),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 2),
+        child: RichText(
+            text: TextSpan(
+                text: titulo,
+                style: const TextStyle(fontSize: 12, color: Colors.white))),
+      )
     ]);
   }
 }
